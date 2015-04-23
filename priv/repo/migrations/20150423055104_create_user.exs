2d879761765ef19fd1329e6b0db6a4a2b91c96fa
Philipp Küng <git@philippkueng.ch>
2015-04-23 07:54:45 +0200

    adding a users table

---
+++ priv/repo/migrations/20150423055104_create_user.exs
@@ -0,0 +1,12 @@
+defmodule Librarian.Repo.Migrations.CreateUser do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :name, :string
+      add :email, :string
+
+      timestamps
+    end
+  end
+end
