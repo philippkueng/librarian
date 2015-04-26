dc4c7c5672d89183b422cceb9c634120f802caa3
Philipp Küng <git@philippkueng.ch>
2015-04-26 18:19:48 +0200

    adding a function to create the database configuration object from the DATABASE_URL environment variable

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -1,5 +1,21 @@
 use Mix.Config
 
+defmodule Heroku do
+  def database_config(uri) do
+    parsed_uri = URI.parse(uri)
+    [username, password] = parsed_uri.userinfo
+                          |> String.split(":")
+    [_, database] = parsed_uri.path
+                    |> String.split("/")
+    [{:username, username},
+     {:password, password},
+     {:host, parsed_uri.host},
+     {:database, database},
+     {:port, parsed_uri.port},
+     {:adapter, Ecto.Adapters.Postgres}]
+  end
+end
+
 # In this file, we keep production configuration that
 # you likely want to automate and keep it away from
 # your version control system.
@@ -8,7 +24,5 @@ config :librarian, Librarian.Endpoint,
 
 # Configure your database
 config :librarian, Librarian.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
-  database: "librarian_prod"
+  System.get_env("DATABASE_URL")
+  |> Heroku.database_config
