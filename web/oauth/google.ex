31946027d76e005a2c287381cc52f3b0005f80d4
Philipp Küng <git@philippkueng.ch>
2015-05-04 07:41:49 +0200

    adding a first oauth implementation for google

---
+++ web/oauth/google.ex
@@ -0,0 +1,42 @@
+defmodule Google do
+  @moduledoc """
+  An OAuth2 strategy for Google.
+  """
+  use OAuth2.Strategy
+
+  alias OAuth2.Strategy.AuthCode
+
+  # Public API
+
+  def new do
+    OAuth2.new([
+      strategy: __MODULE__,
+      client_id: System.get_env("GOOGLE_CLIENT_ID"),
+      client_secret: System.get_env("GOOGLE_CLIENT_SECRET"),
+      redirect_uri: System.get_env("REDIRECT_URI"),
+      site: "",
+      authorize_url: "https://accounts.google.com/o/oauth2/auth",
+      token_url: "https://accounts.google.com/o/oauth2/token"
+    ])
+  end
+
+  def authorize_url!(params \\ []) do
+    OAuth2.Client.authorize_url!(new(), params)
+  end
+
+  def get_token!(params \\ [], headers \\ []) do
+    OAuth2.Client.get_token!(new(), params)
+  end
+
+  # Strategy Callbacks
+
+  def authorize_url(client, params) do
+    AuthCode.authorize_url(client, params)
+  end
+
+  def get_token(client, params, headers) do
+    client
+    |> put_header("Accept", "application/json")
+    |> AuthCode.get_token(params, headers)
+  end
+end
