353db520da2d977d67371a3fb11867cce72e3e92
Philipp Küng <git@philippkueng.ch>
2015-06-05 22:37:54 +0200

    saving all the required google oauth token data with the user into the database

--- web/controllers/authentication/google_controller.ex
+++ web/controllers/authentication/google_controller.ex
@@ -1,5 +1,6 @@
 defmodule Librarian.Authentication.GoogleController do
   use Librarian.Web, :controller
+  alias Librarian.User
 
   plug :action
 
@@ -21,13 +22,32 @@ defmodule Librarian.Authentication.GoogleController do
     # Exchange an auth code for an access token
     token = Google.get_token!(code: code)
 
-    # extract the attributes that need to be saved to the database
+    # Extract the attributes that need to be saved to the database
     %{client: %{client_id: client_id,
                 client_secret: client_secret},
       refresh_token: refresh_token} = token
-    # Request the user's data with the access token
-    user = OAuth2.AccessToken.get!(token, "/oauth2/v1/userinfo")
 
+    # Request the user's data with the access token
+    user_data = OAuth2.AccessToken.get!(token, "/oauth2/v1/userinfo")
+
+    # Fetch the current user from the database
+    user = Repo.get(User, get_session(conn, :current_user))
+    changeset = User.changeset(user, %{google_client_id: client_id,
+                                       google_client_secret: client_secret,
+                                       google_refresh_token: refresh_token})
+
+    if changeset.valid? do
+      Repo.update(changeset)
+
+      conn
+      |> put_flash(:info, "Authenticated with Google successfully.")
+      |> redirect(to: page_path(conn, :index))
+    else
+      conn
+      |> put_flash(:error, "Couldn't authenticate with Google.")
+      |> redirect(to: page_path(conn, :index))
+    end
+    
     # Store the user in the session under `:current_user` and redirect to /.
     # In most cases, we'd probably just store the user's ID that can be used
     # to fetch from the database. In this case, since this example app has no
@@ -36,7 +56,7 @@ defmodule Librarian.Authentication.GoogleController do
     # If you need to make additional resource requests, you may want to store
     # the access token as well.
     conn
-    |> put_session(:current_user_google, user)
+    |> put_session(:current_user_google, user_data)
     |> put_session(:access_token, token.access_token)
     |> redirect(to: "/")
   end
