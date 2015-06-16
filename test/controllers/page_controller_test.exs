24435740563ec4d5b0bb68c31054707623af0517
Philipp Küng <git@philippkueng.ch>
2015-06-16 10:09:36 +0200

    adding a simple hound test case to retrieve the root page

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -1,8 +1,20 @@
 defmodule Librarian.PageControllerTest do
   use Librarian.ConnCase
+  use ExUnit.Case
+  use Hound.Helpers
+
+  hound_session
 
   test "GET /" do
     conn = get conn(), "/"
     assert conn.resp_body =~ "Librarian"
   end
+
+  test "GET / with selenium", meta do
+    navigate_to("/")
+    assert page_title() == "Librarian"
+    assert page_source =~ "Login"
+    assert page_source =~ "Signup"
+  end
+
 end
