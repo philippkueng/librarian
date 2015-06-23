530a2781a8109dca62be18d8e5fa7da9db5023ca
Philipp Küng <git@philippkueng.ch>
2015-06-23 07:26:11 +0200

    add a successful login integration test

--- test/controllers/session_controller_test.exs
+++ test/controllers/session_controller_test.exs
@@ -3,6 +3,8 @@ defmodule Librarian.SessionControllerTest do
   use ExUnit.Case
   use Hound.Helpers
 
+  alias Passport.RegistrationManager
+
   hound_session
 
   test "fill out login form with wrong password" do
@@ -17,5 +19,23 @@ defmodule Librarian.SessionControllerTest do
     submit_element(login_element)
 
     assert visible_text({:class, "alert-warning"}) == "Email or password incorrect."
+  end
+
+  test "fill out login form with correct password" do
+    # Create the test user
+    RegistrationManager.register(%{email: "foo@foo.com",
+                                   password: "foobar"})
+
+    navigate_to("/login")
+
+    login_element = find_element(:id, "session_email")
+    fill_field(login_element, "foo@foo.com")
+
+    password_element = find_element(:id, "session_password")
+    fill_field(password_element, "foobar")
+
+    submit_element(login_element)
+
+    assert visible_text({:class, "alert-warning"}) == "Login succesful."
   end
 end
